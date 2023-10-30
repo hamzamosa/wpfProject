0b28f662224c54bae05325ecab2850b0dadb8454
hamza12 <hamza.a@hybridsolutions.com>
2023-10-30 17:56:03 +0300

    add first

---
+++ HamzaUi/Data/FriendDataServices.cs
@@ -0,0 +1,23 @@
+﻿using HamzaModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HamzaUi.Data
+{
+    class FriendDataServices
+    {
+        public IEnumerable<Friend> GetAll() 
+        {
+
+            yield return new Friend { firstName = "hamza", lastName = "aldaoud" };
+            yield return new Friend { firstName = "mousa", lastName = "mohamed" };
+            yield return new Friend { firstName = "omar", lastName = "khaled" };
+            yield return new Friend { firstName = "monther", lastName = "fadi" };
+
+
+        }
+    }
+}
---
+++ HamzaUi/Data/IfriendDataServices.cs
@@ -0,0 +1,10 @@
+﻿using HamzaModel;
+using System.Collections.Generic;
+
+namespace HamzaUi.Data
+{
+    public interface IfriendDataServices
+    {
+        IEnumerable<Friend> GetAll();
+    }
+}
